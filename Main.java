e3297cd9dc2f49d6b85cd7191127d47dc653957f
Ayush Patange <patangeayush998@gmail.com>
2024-01-12 21:53:43 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,125 @@
+package electricity_bill;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	long currentUnits;     // Declaring Variables for calculations of units
+	long prevMonthUnits;
+	long units;
+
+	public long units(long currentUnits, long prevMonthUnits) // Method to calculate units to pay for
+	{
+		units = currentUnits - prevMonthUnits;
+		return units;
+
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("/==== Electricity Bill Program ====/");
+		System.out.println();
+		System.out.println("Enter Your Name : ");
+		String cusName=sc.nextLine();
+		System.out.println("Enter Your Address : ");
+		String cusAddress=sc.nextLine();
+		System.out.println("Enter Your Mobile Number : ");
+		long cusMobileNumber=sc.nextLong();
+		
+		// Start : Code for calculating units consume in this month
+		
+		Main unit=new Main();
+		System.out.println("Enter your Current Units");
+		long currentUnits=sc.nextLong();
+		System.out.println("Enter your previous Month Units");
+		long prevMonthUnits=sc.nextLong();
+		System.out.println();
+		long u1=unit.units(currentUnits, prevMonthUnits);
+		System.out.println("Units Consume this Month is = " + u1);
+		System.out.println();
+		
+		// End : Code for calculating units consume in this month
+		
+		long units=u1;
+		long amountToPay=0;
+		System.out.println("Press 1 for Domestic Type Connection ");
+		System.out.println("Press 2 For Commercial Type Connection");
+		System.out.println("Press 3 for Exit");
+		
+		System.out.println("Enter Number of your Type of Connection");
+		int type = sc.nextInt();
+		while (true) {
+			
+			if (type == 1) {
+
+				if (units <= 100) {
+					amountToPay = units * 1;
+				} else if (units > 101 && units <= 200) {
+					amountToPay = 100 * 1 + (units - 100) * 2;
+				} else if (units > 201 && units <= 500) {
+					amountToPay = 100 * 1 + 100 * 2 + (units - 200) * 4;
+
+				} else if (units > 501) {
+
+					amountToPay = 100 * 1 + 100 * 2 + 300 * 4 + (units - 500) * 6;
+				}
+				double t1=amountToPay;
+				//System.out.println(amountToPay);
+				break;
+
+			} 
+			else if (type == 2) {
+				if (units <= 100) {
+					amountToPay = units * 2;
+				} else if (units > 101 && units <= 200) {
+					amountToPay = 100 * 1 + (units - 100) * 4;
+				} else if (units > 201 && units <= 500) {
+					amountToPay = 100 * 1 + 100 * 2 + (units - 200) * 6;
+
+				} else if (units > 501) {
+
+					amountToPay = 100 * 1 + 100 * 2 + 300 * 4 + (units - 500) * 7;
+				}
+				double t2=amountToPay;
+				//System.out.println(amountToPay);
+				break;
+			}
+			else if (type == 3) {
+                break;
+			}
+			else {
+				System.out.println("Inavlid Input");
+			}		
+		}
+		
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("==== Your Electricity Bill Had Been Generated ====");
+		System.out.println();
+		System.out.println();
+		System.out.println(" Welcome Mr/Ms : "+cusName.toUpperCase());
+		System.out.println(" Address : "+cusAddress);
+		System.out.println(" Mobile Number : " +cusMobileNumber);
+		System.out.println(" Previous Month Reading "+prevMonthUnits);
+		System.out.println(" Current Month Reading "+currentUnits);
+		System.out.println(" Units You Consumed This Month "+units);
+		
+		if(type==1) {
+			System.out.println(" Your Connection Type is Domestic Type ");
+		}
+		else if(type == 2) {
+			System.out.println(" Your Connection Type is Commercial Type ");
+		}
+		
+		System.out.println(" You Have To Pay : "+amountToPay +"Rs");
+		System.out.println();
+		System.out.println(" Thanks For Visiting");
+		
+		
+
+	}
+
+}
+
